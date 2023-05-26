c8cdcac19689b81b9c5e42b5479eeb03e0b69c79
dmitr_zubakin <daz371885bb@gmail.com>
2023-05-26 14:00:50 +0200

    feature: add loader and errorPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,30 @@
-import Header from "./components/organisms/header/Header.jsx";
 import ExploreRecent from "./components/organisms/exploreRecent/ExploreRecent.jsx";
 import HeadingPage from "./components/organisms/headingPage/HeadingPage.jsx";
 import Creator from "./components/molecules/creator/Creator.jsx";
 import {useFetchData} from "./hooks/UseFetchData.js";
 import {API_URL} from "./helpers/fetchData.js";
 import LiveAuctions from "./components/organisms/liveAuctions/LiveAuctions.jsx";
-import styles from './App.module.scss'
-import useThemeStore from "./providers/themeStore";
+import Layout from "./components/templates/Layout";
+import Loader from "./components/molecules/loader/Loader";
+import ErrorPage from "./components/organisms/errorPage/ErrorPage";
 
 function App() {
     const {data, loading} = useFetchData(`${API_URL}/nft`)
-    const { isDark } = useThemeStore();
 
     if(loading){
-        return <div>loading...</div>
+        return <Loader/>
     }
     if(!data){
-        return <div>ERROR</div>
+        return <ErrorPage/>
     }
       return (
           <>
-                <div className={isDark ? styles.dark : styles.light}>
-                     <Header/>
-                     <HeadingPage id='home'/>
-                     <Creator id='creators'/>
+                <Layout>
+                    <HeadingPage id='home' data={data}/>
+                    <Creator id='creators'/>
                     <ExploreRecent data={data} id='marketplace'/>
-                    <LiveAuctions/>
-                </div>
+                    <LiveAuctions data={data}/>
+                </Layout>
           </>
       )
     }
