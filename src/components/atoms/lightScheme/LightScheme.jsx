558c8139d6639788ce11fa77df4d65267b2c1c2d
dmitr_zubakin <daz371885bb@gmail.com>
2023-05-26 13:20:58 +0200

    fix: update LightScheme

--- src/components/atoms/lightScheme/LightScheme.jsx
+++ src/components/atoms/lightScheme/LightScheme.jsx
@@ -2,14 +2,12 @@
 import InlineSVG from "react-inlinesvg";
 import lightIcon from "../../../assets/icons/sun 1.svg";
 import styles from "./LightScheme.module.scss";
-import {useContext} from "react";
-import {ThemeContext} from "../../../providers/ThemeContext";
+
 const LightScheme = ({handleClick}) => {
 
-    const { toggleTheme } = useContext(ThemeContext);
     return (
         <div className={styles.lightScheme}>
-            <button className={styles.button} onClick={handleClick || toggleTheme}>
+            <button className={styles.button} onClick={handleClick}>
                 <InlineSVG src={lightIcon}/>
             </button>
         </div>
